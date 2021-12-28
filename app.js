8319fee4559f21a86663f0d755ad3edbbce5bbe7
superxiaodian <supercq1980@qq.com>
2021-12-28 13:25:10 +0800

    ddd

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 
-app.listen(3000, ()=>{
-    console.log("3000端口已开启");
+
+//connect mongodb
+dbURI = "mongodb://nodeapi:chenqing123%40%40@49.235.95.188:27017/nodeapi"
+
+mongoose.connect(dbURI)
+    .then(() => {
+        console.log("数据库已连接")
+        app.listen(3000)
+    })
+    .catch(error => console.log(error))
+
+
+app.get("/", (req, res)=>{
+    res.send("<p>hello</p>")
 })
